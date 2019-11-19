b078fefb32724558cb0f42b5f2cc0b4bb539ea44
monte <greg.jonca@gmail.com>
2019-11-19 16:00:46 +0100

    Create project for notifier service app

---
+++ src/notifier/Services/NotifierService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace Notifier.Services
+{
+    public class NotifierService : Notifier.NotifierBase
+    {
+        private readonly ILogger<NotifierService> _logger;
+
+        public NotifierService(ILogger<NotifierService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<NotifyReply> Notify(NotifyRequest request, ServerCallContext context)
+        {
+            _logger.LogInformation($"Sending notification to user: ");
+            return base.Notify(request, context);
+        }
+    }
+}
